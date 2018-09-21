75ff6f5aae619367754ebc4c6e8310d1fd9e8bc7
Gleb <zadachings@gmail.com>
2018-09-21 22:44:01 +0300

    created lab

--- Lab3_SherbashinWebForms/Form1.Designer.cs
+++ Lab3_SherbashinWebForms/Form1.Designer.cs
@@ -28,13 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dtForm = new System.Windows.Forms.ComboBox();
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtForm
+            // 
+            this.dtForm.FormattingEnabled = true;
+            this.dtForm.Items.AddRange(new object[] {
+            "0.1",
+            "0.2",
+            "1",
+            "5",
+            "20"});
+            this.dtForm.Location = new System.Drawing.Point(613, 12);
+            this.dtForm.Name = "dtForm";
+            this.dtForm.Size = new System.Drawing.Size(121, 24);
+            this.dtForm.TabIndex = 1;
+            this.dtForm.SelectedIndexChanged += new System.EventHandler(this.dtForm_SelectedIndexChanged);
+            // 
+            // grid
+            // 
+            this.grid.AllowUserToAddRows = false;
+            this.grid.AllowUserToDeleteRows = false;
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.Location = new System.Drawing.Point(12, 12);
+            this.grid.Name = "grid";
+            this.grid.ReadOnly = true;
+            this.grid.RowTemplate.Height = 24;
+            this.grid.Size = new System.Drawing.Size(545, 426);
+            this.grid.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(583, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "dt:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(613, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 40);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(784, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.grid);
+            this.Controls.Add(this.dtForm);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox dtForm;
+        private System.Windows.Forms.DataGridView grid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Lab3_SherbashinWebForms/Form1.cs
+++ Lab3_SherbashinWebForms/Form1.cs
@@ -12,9 +12,150 @@ namespace Lab3_SherbashinWebForms
 {
     public partial class Form1 : Form
     {
+
+        const float q = 1700;
+        const float Vg = 3800;
+        const float m0 = 200000;
+        const float Re = 6371000;
+        float Me = 5.96f * (float)Math.Pow(10, 24);
+        float KG = 6.67f * (float)Math.Pow(10, -11);
+        float[] dt = { 0.1f, 0.2f, 1, 5, 20 };
+
+        float F = 0;
+        float Fprev;
+        float P = 0;
+        float Pprev = 0;
+        float m = m0;
+        float mprev = m0;
+        float r = 0;
+        float rprev;
+        float x = 0;
+        float xprev = 0;
+        float a = 0;
+        float aprev = 0;
+        float V = 0;
+        float Vprev = 0;
+
+        float d;
+
+        float GetF()
+        {
+            F = Vg * q - Pprev;
+            Fprev = F;
+            return F;
+        }
+
+        float GetP()
+        {
+            P = KG * ((Me * mprev) / (float)Math.Pow(rprev, 2));
+            Pprev = P;
+            return P;
+        }
+
+        float GetV()
+        {
+            V = Vprev + (F / mprev) * d;
+            Vprev = V;
+            return V;
+        }
+
+        float GetX()
+        {
+            x = xprev + V * d;
+            xprev = x;
+            return x;
+        }
+
+        float GetR()
+        {
+            r = Re + x;
+            rprev = r;
+            return r;
+        }
+
+        float GetM()
+        {
+            m = mprev - q * d;
+            mprev = m;
+            return m;
+        }
+
+        float GetA()
+        {
+            a = F / m;
+            aprev = a;
+            return a;
+        }
+
+        void UpdateValues()
+        {
+            GetM();
+            GetX();
+            GetR();
+            GetP();
+            GetF();
+            GetA();
+            GetV();
+        }
+        
+        void Reset()
+        {
+            F = 0;
+            Fprev = 0;
+            P = 0;
+            Pprev = 0;
+            m = m0;
+            mprev = m0;
+            r = 0;
+            rprev = 0;
+            x = 0;
+            xprev = 0;
+            a = 0;
+            aprev = 0;
+            V = 0;
+            Vprev = 0;
+        }
+
         public Form1()
         {
             InitializeComponent();
+            dtForm.SelectedIndex = 0;
+            grid.Columns.Add("i", "i c");
+            grid.Columns.Add("a", "a м/с2");
+            grid.Columns.Add("v", "v м/с");
+            grid.Columns.Add("x", "x м");
+            grid.AutoResizeColumns();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        void GetResults()
+        {
+            grid.Rows.Clear();
+            for (float i = 0; i <= 60; i += d)
+            {
+                UpdateValues();
+                grid.Rows.Add(i, a, V, x);
+            }
+        }
+
+        private void dtForm_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            d = dt[dtForm.SelectedIndex];
+            Reset();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetResults();
         }
     }
 }
